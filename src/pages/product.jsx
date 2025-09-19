7ca2970ca9a2f0eb0e0cf68e5555e466df345a32
rahimmsz <150146538+rahimmsz@users.noreply.github.com>
2025-09-19 14:45:54 +0800

    Use State

--- src/pages/product.jsx
+++ src/pages/product.jsx
@@ -1,6 +1,7 @@
 import { Fragment } from 'react';
 import CardProduct from '../components/Fragments/CardProduct';
 import Button from '../components/Elements/Button';
+import Counter from '../components/Fragments/Counter';
 
 const products = [
   {
@@ -61,6 +62,10 @@ const ProductsPage = () => {
             <CardProduct.Footer price={product.price} />
           </CardProduct>
         ))}
+      </div>
+
+      <div className="flex justify-center">
+        <Counter />
       </div>
     </Fragment>
   );
